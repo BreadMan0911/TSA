acf98677e4ed2fa94ad5cc99fc39b71dbeb957a2
DillAssassin969 <123483884+DillAssassin969@users.noreply.github.com>
2023-02-10 09:29:20 -0600

    02/10/2023

---
+++ TSA/Assets/Scripts/Player3.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player3 : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public bool jump = true;
+    public float jumpForce = 5.0f;
+    public int health = 1;
+    int currentHealth;
+    public Transform respawnPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = health;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+         if (Input.GetKey(KeyCode.J))  
+         {
+             transform.Translate(Vector2.left * Time.deltaTime * speed);
+         }
+         if (Input.GetKey(KeyCode.L))
+         {
+             transform.Translate(Vector2.right * Time.deltaTime * speed);
+         }
+         if (Input.GetKey(KeyCode.I) && jump != false)
+         {
+             transform.Translate(Vector2.up * Time.deltaTime * jumpForce);
+         }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == ("Ground"))
+        {
+            jump = true;
+        }
+    }
+    void OnCollisionExit(Collision other)
+    {
+        if(other.gameObject.tag == ("Ground"))
+        {
+            jump = false;
+        }
+    }
+    public void ChangeHealth(int amount) 
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, health);
+        if (amount < 0)
+        {
+            if(currentHealth == 0)
+            Respawn();
+        }
+    }
+    void Respawn()
+    {
+        ChangeHealth(health);
+        transform.position = respawnPosition.position;
+    }
+}
